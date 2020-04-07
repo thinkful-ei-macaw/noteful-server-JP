5049c945c2e79a50032b5085a5e59ed69f8cf4c4
Patrick Nguyen <patrick.nguyen154@gmail.com>
2020-04-07 13:57:20 -0600

    working on test

--- src/note-router.js
+++ src/note-router.js
@@ -1,118 +1,109 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const notesRouter = express.Router();
-const notesService = require('./notes-service');
+const notesService = require("./notes-service");
 
 // create note
-notesRouter
-  .route('/')
-  .post((req, res, next) => {
-    req.app.get('db');
 
-    const fieldsRequired = ['name', 'content', 'folder_id'];
+const serializeNotes = (note) => ({
+  id: note.id,
+  title: note.title,
+  content: note.content,
+  date: note.date,
+  folder_id: Number(note.folder_id),
+});
+
+notesRouter
+  .route("/")
+  .get((req, res, next) => {
+    notesService
+      .getAllNotes(req.app.get("db"))
+      .then((notes) => {
+        res.json(notes.map(serializeNotes));
+      })
+      .catch(next);
+  })
+  .post((req, res, next) => {
+    req.app.get("db");
+
+    const fieldsRequired = ["name", "content", "folder_id"];
     for (let field in fieldsRequired) {
       if (!req.body[field]) {
-        return res
-          .status(400)
-          .send(`'${field} is required`);
+        return res.status(400).send(`'${field} is required`);
       }
     }
-    const {
-      id,
-      title,
-      content,
-      folder_id,
-      date
-    } = req.body;
+    const { id, title, content, folder_id, date } = req.body;
 
     const note = {
       title,
       content,
-      folder_id
+      folder_id,
     };
 
     if (id) note.id = id;
     if (date) note.date = date;
 
-    notesService.insertNote(
-      note);
-    req.app.get('db')
-      .then(note => {
+    notesService.insertNote(note);
+    req.app
+      .get("db")
+      .then((note) => {
         return res.json(note);
       })
       .catch(next);
   });
 
-
 // read notes, update notes, delete
 notesRouter
-  .get('/:id', (req, res, next) => {
-    const {
-      id
-    } = req.params;
-    const db = req.app.get('db');
+  .get("/:id", (req, res, next) => {
+    const { id } = req.params;
+    const db = req.app.get("db");
 
-    notesService.getById(db, id)
-      .then(note => {
+    notesService
+      .getById(db, id)
+      .then((note) => {
         if (note) {
           return res.status(200).json(note);
         } else {
-          return res.status(400).send('Note not found');
+          return res.status(400).send("Note not found");
         }
       })
       .catch(next);
   })
   .patch((req, res, next) => {
-    const {
-      id,
-      title,
-      content,
-      date
-    } = req.body;
+    const { id, title, content, date } = req.body;
 
     const noteToUpdate = {
       id,
       title,
       content,
-      date
+      date,
     };
 
     if (!title) {
-      return res.status(404).json({error: 'must include title'});
+      return res.status(404).json({ error: "must include title" });
     }
 
     if (!content) {
       return res.status(404).json({
-        error: 'must include content'
+        error: "must include content",
       });
     }
 
-    notesService.updateNote(req.app.get('db'),
-      noteToUpdate)
-      .then(noteToUpdate => {
+    notesService
+      .updateNote(req.app.get("db"), noteToUpdate)
+      .then((noteToUpdate) => {
         res.json(noteToUpdate);
       })
       .catch(next);
   })
   .delete((req, res, next) => {
-    notesService.deleteNote(
-      req.app.get('db'),
-      req.params.id
-    )
+    notesService
+      .deleteNote(req.app.get("db"), req.params.id)
       .then(() => {
         res.status(204).end();
       })
       .catch(next);
   });
-
-
-
-
-
-
-
-
-
 
 module.exports = notesRouter;
--- test/notes-endpoints.spec.js
+++ test/notes-endpoints.spec.js
@@ -14,64 +14,69 @@ describe("Notes Endpoints", function () {
 
   after("disconnect from db", () => db.destroy());
 
-  before("clean the table", () => db("noteful-test").truncate());
+  before("clean the table", () =>
+    db("notes")
+      .truncate()
+      .then(() => db.raw("TRUNCATE TABLE folders RESTART IDENTITY CASCADE"))
+  );
+
+  afterEach("cleanup", () =>
+    db("notes")
+      .truncate()
+      .then(() => db.raw("TRUNCATE TABLE folders RESTART IDENTITY CASCADE"))
+  );
 
   context("Given there are notes in the database", () => {
-    const testNotes = makeNotesArray;
-
-    this.beforeEach("insert notes", () => {
-      return db.into("noteful-test").insert(testNotes)
-      .then(()=> {
-return db
-.into('notes')
-.insert(testNotes)
-      })
-    });
-it('responds with 200 and all of the notes', ()=>{
-  return supertest(app)
-  .get('/notes')
-  .expect(200, testNotes)
-})
-
-  });
-// context(`Given an XSS attack note`, ()=> {
-//   const testNotes = makeNotesArray();
-//   const { maliciousNote, expectedNote } = makeMaliciousNote()
-
-//   beforeEach('insert malicious note', () => {
-//     return db
-//     .into('notes')
-//     .insert(testNotes)
-//     .then(()=>{
-//       return db
-//       .into('notes')
-//       .insert([maliciousNote])
-//     })
-//   })
-
-//   it('removes XSS attack content', ()=>{
-//     return supertest(app)
-//     .get('/notes')
-//     .expect(200)
-//     .expect(res => {
-//       expect(res.body[0].title).to.eql(expectedNote.title)
-//       expect(res.body[0].content).to.eql(expectedNote.content)
-//     })
-//   })
-// })
-
-})
-describe (`GET /notes/:notes_id`, () =>{
-  context(`Given no notes`, ()=>{
-    it(`responds with 404`, () => {
-      const noteId = 123456
-      return supertest(app)
-      .get(`/notes/${noteId}`)
-      .expect(404, {error: {message: `Note doesn't exist`}})
-    })
-  })
-
-  context ('Given there are articles in the database', () => {
+    const testFolders = makeFoldersArray();
     const testNotes = makeNotesArray();
-  })
-})
+
+    beforeEach("insert folders", () => {
+      return db.insert(testFolders).into("folders");
+    });
+    beforeEach("insert notes", () => {
+      return db.insert(testNotes).into("notes");
+    });
+    it.only("responds with 200 and all of the notes", () => {
+      return supertest(app).get("/note").expect(200, testNotes);
+    });
+  });
+  // context(`Given an XSS attack note`, ()=> {
+  //   const testNotes = makeNotesArray();
+  //   const { maliciousNote, expectedNote } = makeMaliciousNote()
+
+  //   beforeEach('insert malicious note', () => {
+  //     return db
+  //     .into('notes')
+  //     .insert(testNotes)
+  //     .then(()=>{
+  //       return db
+  //       .into('notes')
+  //       .insert([maliciousNote])
+  //     })
+  //   })
+
+  //   it('removes XSS attack content', ()=>{
+  //     return supertest(app)
+  //     .get('/notes')
+  //     .expect(200)
+  //     .expect(res => {
+  //       expect(res.body[0].title).to.eql(expectedNote.title)
+  //       expect(res.body[0].content).to.eql(expectedNote.content)
+  //     })
+  //   })
+  // })
+});
+describe(`GET /notes/:notes_id`, () => {
+  context(`Given no notes`, () => {
+    it(`responds with 404`, () => {
+      const noteId = 123456;
+      return supertest(app)
+        .get(`/notes/${noteId}`)
+        .expect(404, { error: { message: `Note doesn't exist` } });
+    });
+  });
+
+  context("Given there are articles in the database", () => {
+    const testNotes = makeNotesArray();
+  });
+});
